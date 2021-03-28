92ea0f4f318d35c341dcbb5d28a12c3a0702f7c2
TJ <tj.borges3@gmail.com>
2021-03-28 14:38:11 -0300

    Adicionando regras de geração
    CheckList e Limite de caracteres

--- AppComIcones/ViewController.swift
+++ AppComIcones/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tfTotalPasswords: UITextField!
     @IBOutlet weak var tfNumberOfCharacters: UITextField!
@@ -17,10 +17,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var swSpecialCharacters: UISwitch!
     @IBOutlet weak var swCaptitalLetters: UISwitch!
     
+    @IBOutlet weak var btnGerarSenha: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tfTotalPasswords.delegate = self
+        self.tfNumberOfCharacters.delegate = self
     }
     
+    
+    @IBAction func checkList(_ sender: Any) {
+        if(!swLetters.isOn && !swNumbers.isOn && !swSpecialCharacters.isOn && !swCaptitalLetters.isOn ){
+            //btnGerarSenha.isHidden = true
+            btnGerarSenha.isEnabled = false
+        }
+        else{
+            //btnGerarSenha.isHidden = false
+            btnGerarSenha.isEnabled = true
+
+        }
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let maxLength = 2
+        let currentString: NSString = (textField.text ?? "") as NSString
+        let newString: NSString =
+            currentString.replacingCharacters(in: range, with: string) as NSString
+        return newString.length <= maxLength
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?){
         let passwordsViewController = segue.destination as! PasswordsViewController
